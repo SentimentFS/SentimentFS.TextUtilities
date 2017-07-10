26d9166beaab3a75cad9c4db6b0ce517dd155c29
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2017-07-10 12:25:37 +0200

    seq

--- src/SentimentFS.TextUtilities/Filter.fs
+++ src/SentimentFS.TextUtilities/Filter.fs
@@ -1,5 +1,8 @@
 namespace SentimentFS.TextUtilities
 
-module Filter = 
+module Filter =
     let filterOut(filterList: string list)(list: string list) =
         list |> List.filter(fun x -> not (filterList |> List.exists(fun y -> x = y)))
+
+    let filterOutSeq(filterSeq: string seq)(list: string seq) =
+        filterOut(filterSeq |> Seq.toList)(list |> Seq.toList) |> List.toSeq
